cab6ee601ac682b0608a67e80c8c53ced67448ce
sahil-sheikh-17 <63206410+sahil-sheikh-17@users.noreply.github.com>
2022-09-10 19:22:31 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  access_key              = "AKIATGPOXLRZGWD3JA7A"
+  secret_key              = "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"  
+  profile                 = "default"
+  region                  = "us-east-1"
+}
