dafcb85c609c430d559d628b6f5057e5d3a5feb6
sahil-sheikh-17 <63206410+sahil-sheikh-17@users.noreply.github.com>
2022-09-13 12:31:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,8 +7,6 @@ terraform {
 }
 
 provider "aws" {
-  #access_key              = ""
- # secret_key              = ""  
   profile                 = "default"
   region                  = "us-east-1"
 }
